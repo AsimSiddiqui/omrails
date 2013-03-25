22818a57f902a3624caabe028e2c0436983f756d
Asim Siddiqui <asim@wizenation.com>
2013-03-25 19:16:34 -0400

    Add Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,15 @@ Omrails::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+# Paperclip should use Amazon S3 on Heroku
+config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+        :bucket => "asimonemonthrails",
+        :access_key_id => "AKIAJGLCQS4QHLCPYSIQ",
+        :secret_access_key => "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"
+    }
+}
+
 end
